8f03b832a0fa8d2af15caa7e4afcf2ad9ec0abdf
Franco484 <71747272+Franco484@users.noreply.github.com>
2025-01-17 17:08:32 +0700

    Database

---
+++ routes/TB_Admin.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Admin (
+    admin_id INT PRIMARY KEY,
+    admin_name VARCHAR(100) NOT NULL
+);
---
+++ routes/TB_Pengguna.sql
@@ -0,0 +1,4 @@
+CREATE TABLE User (
+    user_id INT PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL
+);
